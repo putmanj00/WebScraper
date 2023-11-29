8db946714d245d5058bd001a88365a36ff96a1e8
putmanj00 <putmanj00@hotmail.com>
2023-11-29 01:17:45 -0500

    o

--- newSnak.py
+++ newSnak.py
@@ -12,7 +12,7 @@ import re
 url = "https://en.seedfinder.eu/database/strains/alphabetical/"
 
 # Alphabetical list of pages for strains
-strainAlphabeticalList = ["l"]
+strainAlphabeticalList = ["o"]
 
 # Create a new Excel workbook
 workbook = openpyxl.Workbook()
