1c67a91a19700d1cede32eee2df0377bdc862a11
putmanj00 <putmanj00@hotmail.com>
2023-12-11 03:32:53 -0500

    tet

--- newSnak.py
+++ newSnak.py
@@ -14,8 +14,10 @@ def get_description(url):
     description = ' '.join(paragraph.get_text(strip=True) for paragraph in paragraphs)
     return description
 
-# Set the version of Chromedriver to use (update this based on Chrome browser version)
-chromedriver_version = "120.0.6099.71"
+# Download the latest Chromedriver version
+os.system("wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE -O chromedriver_version.txt")
+with open("chromedriver_version.txt", "r") as file:
+    chromedriver_version = file.read().strip()
 
 # Download Chromedriver
 chromedriver_url = f"https://chromedriver.storage.googleapis.com/{chromedriver_version}/chromedriver_linux64.zip"
@@ -38,68 +40,3 @@ except Exception as e:
 
 # URL for the "x" page
 url = "https://en.seedfinder.eu/database/strains/alphabetical/x/"
-
-# Send an HTTP request to the URL
-response = requests.get(url)
-
-# Parse the HTML content of the page
-soup = BeautifulSoup(response.content, "html.parser")
-
-# Find the cannabis strain table
-table = soup.find("table", {"id": "cannabis-strain-table"})
-
-# Lists to store data
-strains = []
-breeders = []
-indica_sativa_list = []
-indoor_outdoor_list = []
-flowering_time_list = []
-female_seeds_list = []
-descriptions = []
-
-# Iterate over each table row
-for row in table.find_all("tr"):
-    header = row.find("th", {"class": "xs1"})
-    if header:
-        link = header.find("a")
-        if link:
-            strain = link.get_text(strip=True)
-            breeder = link["title"].split("(")[-1].strip(")")
-
-            try:
-                indica_sativa = row.find_element(By.CSS_SELECTOR, "td img[width='20']").get_attribute("title")
-                indoor_outdoor = row.find_element(By.CSS_SELECTOR, "td.x20 img[height='14']").get_attribute("title")
-                flowering_time = row.find_element(By.CSS_SELECTOR, "td.graukleinX span").text
-                female_seeds = row.find_element(By.CSS_SELECTOR, "td img[width='12']").get_attribute("title")
-
-                strain_url = f"https://en.seedfinder.eu/{link['href']}"
-                description = get_description(strain_url)
-                descriptions.append(description)
-
-                strains.append(strain)
-                breeders.append(breeder)
-                indica_sativa_list.append(indica_sativa)
-                indoor_outdoor_list.append(indoor_outdoor)
-                flowering_time_list.append(flowering_time)
-                female_seeds_list.append(female_seeds)
-
-            except AttributeError as e:
-                print(f"Failed to process data for strain '{strain}'. Error: {e}")
-                continue
-
-# Create a DataFrame from the lists
-df = pd.DataFrame({
-    'Strain': strains,
-    'Breeder': breeders,
-    'Indica or Sativa': indica_sativa_list,
-    'Indoor or Outdoor': indoor_outdoor_list,
-    'Flowering Time(Days)': flowering_time_list,
-    'Female Seeds(?)': female_seeds_list,
-    'Description': descriptions
-})
-
-# Save the DataFrame to an Excel spreadsheet
-excel_writer = pd.ExcelWriter("cannabis_strains_data.xlsx", engine="xlsxwriter")
-df.to_excel(excel_writer, sheet_name="Cannabis Strains", index=False)
-excel_writer.save()
-print("Data saved to cannabis_strains_data.xlsx")
